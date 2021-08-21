83dfbd8f50390aa56a5e3bcc1b15c21152976f03
fdev31 <fdev31@gmail.com>
2021-08-21 19:03:00 +0200

    remove old code

--- src/main/vue_barbutton/script.js
+++ src/main/vue_barbutton/script.js
@@ -2,13 +2,6 @@ export default {
   mounted() {
     this.caption = this.$attrs.caption;
   },
-  /*
-    watch: {
-        caption(oldVal, newVal) {
-            this.$refs.butlink.attributes['data-caption'] =  newVal;
-        }
-    },
-    */
   data() {
     return {
       caption: "Some text",
