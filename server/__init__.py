85f3fad0fee75e620a57005bc8e9b6007903d2bd
fdev31 <fdev31@gmail.com>
2022-06-13 20:34:22 +0200

    Basic server side OAUTH2 support

--- server/__init__.py
+++ server/__init__.py
@@ -5,15 +5,22 @@ DEFAULT_DBDIR = "./myKB/"
 
 import os
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 import asyncio
 import aiofiles
 
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Depends, HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import RedirectResponse
 from fastapi.responses import ORJSONResponse
+
+
+class Settings:
+    ALGORITHM = "HS256"
+    SECRET_KEY = "kHi37vNk93Dd1LgieCGk+d1axrrQYJ6bM4OEqtu78HA="
+    ACCESS_TOKEN_EXPIRE_MINUTES = 60.0 * 36  # 36 hours
+
 
 try:
     import orjson
@@ -36,6 +43,84 @@ if not os.path.exists(IMAGE_PATH):
     os.mkdir(IMAGE_PATH)
 
 USE_GIT = os.path.exists(os.path.join(PATH, ".git"))
+
+
+# AUTHENTICATION
+
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from datetime import timedelta, datetime
+from jose import JWTError, jwt  # type: ignore
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(
+            minutes=Settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, Settings.SECRET_KEY, algorithm=Settings.ALGORITHM
+    )
+    return encoded_jwt
+
+
+def get_db():
+    return {
+        "fab": "pass",
+    }
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+)
+
+
+@app.post("/token", response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: dict = Depends(get_db)
+):
+    if db.get(form_data.username) == form_data.password:
+        access_token_expires = timedelta(minutes=Settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": form_data.username}, expires_delta=access_token_expires
+        )
+        return {"access_token": access_token, "token_type": "bearer"}
+    else:
+        raise credentials_exception
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
+
+def get_current_user_from_token(
+    token: str = Depends(oauth2_scheme), db: dict = Depends(get_db)
+):
+    try:
+        payload = jwt.decode(
+            token, Settings.SECRET_KEY, algorithms=[Settings.ALGORITHM]
+        )
+        username: str = payload.get("sub")
+        print("username/email extracted is ", username)
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = db.get(username)
+    if user is None:
+        raise credentials_exception
+    return dict(name=username)
+
+
+# END OF AUTHENTICATION CODE
 
 
 async def _gitCmd(*args):
@@ -100,22 +185,31 @@ def index():
     return RedirectResponse(url="/index.html")
 
 
-@app.post("/upload")
-async def upload(file: UploadFile = File(...)):
+@app.post(
+    "/upload",
+)
+async def upload(
+    file: UploadFile = File(...),
+    current_user: dict = Depends(get_current_user_from_token),
+):
     fullname = os.path.join(IMAGE_PATH, file.filename)
     async with aiofiles.open(fullname, "wb") as f:
         await f.write(await file.read())
 
 
 @app.delete("/notebook")
-async def deleteNote(name: str):
+async def deleteNote(
+    name: str, current_user: dict = Depends(get_current_user_from_token)
+):
     """Remove one note"""
     await gitRemove(name)
     await gitSave(name, f"Removed {name}")
 
 
 @app.post("/notebook")
-async def addNote(note: Note):
+async def addNote(
+    note: Note, current_user: dict = Depends(get_current_user_from_token)
+):
     """Create one note"""
     assert "." not in note.name
     assert not os.path.exists(note.filename)
@@ -123,14 +217,18 @@ async def addNote(note: Note):
 
 
 @app.put("/notebook")
-async def updateNote(note: Note):
+async def updateNote(
+    note: Note, current_user: dict = Depends(get_current_user_from_token)
+):
     """Update one note"""
     assert os.path.exists(note.filename)
     await note.save()
 
 
 @app.get("/notebooks")
-async def getNotes() -> List[Note]:
+async def getNotes(
+    current_user: dict = Depends(get_current_user_from_token),
+) -> List[Note]:
     """Fetches the notebook"""
     entries = []
     for root, _dirs, files in os.walk(PATH):
